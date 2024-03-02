71744a029fea766791d9c96dd9f96b6dbc762e3f
Sadaf Ali <134446017+Sadaf-ali34@users.noreply.github.com>
2024-03-02 22:07:26 +0000

    Converstion Operations done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as new version
+// Data types
+
+let name = "sadaf"
+let age = 20
+let isLoggedIn = true
+let state;
+
+// number => 2 to power 53
+// bigint = just like long 
+// string => "" can also use single ''
+// boolean => true/false
+// null => standalone value //object
+// undefined => undifined
+// symbol => unique
+console.table([name, age, isLoggedIn,state])
+
+console.log(typeof undefined) //output -> undefined
+
+console.log(typeof null)// output -> object
---
+++ 01_basics/03_converstionOperation.js
@@ -0,0 +1,32 @@
+let score = 33
+
+// const {score}= Req.body --> coming from backend no specification of the backend
+//console.log(typeof score)
+
+let valueToNumber = Number(score)
+//console.log(typeof score)// score is of numer type 
+//console.log(valueToNumber)// output --33
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "sadaf"
+console.log(typeof isLoggedIn);
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let  randomNum = 28
+let stringNumber = String(randomNum)
+console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+
+
+
