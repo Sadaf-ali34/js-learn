df4e347162c560d4d5f71e70c0a895fdf360cc54
Sadaf Ali <134446017+Sadaf-ali34@users.noreply.github.com>
2024-03-07 23:16:03 +0000

    data type compariosion done

--- 01_basics/03_converstionOperation.js
+++ 01_basics/03_converstionOperation.js
@@ -11,10 +11,10 @@ let valueToNumber = Number(score)
 //  true => 1; false => 0
 
 let isLoggedIn = "sadaf"
-console.log(typeof isLoggedIn);
+// console.log(typeof isLoggedIn);
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -22,10 +22,40 @@ console.log(booleanIsLoggedIn);
 
 let  randomNum = 28
 let stringNumber = String(randomNum)
-console.log(stringNumber)
+// console.log(stringNumber)
 // console.log(typeof stringNumber);
 
 // *********************** Operations ***********************
+
+ let str1 = "hello"
+ let str2= "world"
+ let str3 = str1 + str2
+
+// //console.log(str3)
+
+// console.log("1"+ 2)
+// // o/p -12
+
+// console.log(1 + "2") 
+// //o/p -12
+
+// console.log("1" +2 +2) 
+// // o/p -12
+
+// console.log(1 +2 + "2") 
+// //o/p -12
+
+// console.log(1 +2 + "2") 
+
+// console.log(3 +4  * 5 % 6) 
+// //o/p -> 5
+
+// console.log((3 +4)  * 5 % 6) 
+// //o/p -> 5
+
+
+
+
 
 
 
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,11 @@
+// Data Type conversion
+
+// will take 2 as a number to compare , it should not be done
+// console.log("2"> 1);
+// console.log("02"> 1);
+
+//>,>=,== works differently 
+//comparision converts null to a number treating it as 0
+console.log(null>0);
+console.log(null==0)
+console.log(null>=0)
